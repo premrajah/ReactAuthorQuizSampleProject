222fdb133be31977d5166e2639b0cc94dbf7c52e
Prem <prem.rajah@gmail.com>
2020-05-10 22:34:11 +0100

    added Enzyme and adapter to AuthQuiz.test.js configured enzyme

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme, { mount, shallow, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
 // Grouping of tests
 describe('Author Quiz', () => {
